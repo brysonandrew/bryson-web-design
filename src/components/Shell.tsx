a050f8aa2e6e3c0a41071a86e41f9b478db6e414
Andrew Bryson <andrewbryson12@gmail.com>
2023-03-09 09:06:15 +0100

    Technical issues

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -5,6 +5,8 @@ import type { TChildren } from "../types";
 import { Background } from "./shell/background";
 import { Header } from "./shell/header";
 import { Footer } from "./shell/Footer";
+import { BlindersOut } from "./blinders/BlindersOut";
+import { HEADER_OFFSET_Y } from "@pages/about/constants";
 
 const Root = styled(motion.div)``;
 
@@ -12,10 +14,20 @@ type TProps = {
   children: TChildren;
 };
 export const Shell: FC<TProps> = ({ children }) => (
-  <>
+  <div className="text-black-dark-04">
     <Background />
     <Header />
-    {children}
+    <Root
+      className="relative bg-current mx-auto px-0 w-full overflow-hidden sm:overflow-visible md:w-mid lg:w-1/2 xl:w-2/3"
+      style={{
+        paddingTop: HEADER_OFFSET_Y,
+        minHeight: "100vh",
+      }}
+    >
+      {children}
+
+      <BlindersOut />
+    </Root>
     <Footer />
-  </>
+  </div>
 );
