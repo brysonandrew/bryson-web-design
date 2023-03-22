a6b643f5c49c5fdc871401b807589710e29fcaca
Andrew Bryson <andrewbryson12@gmail.com>
2023-03-22 07:23:01 +0100

    added back cursor

--- src/main/Router.tsx
+++ src/main/Router.tsx
@@ -6,17 +6,18 @@ import { Cursor } from "@components/cursor";
 import { useNoPointer } from "@hooks/useNoPointer";
 import { Background } from "@components/background";
 import { Filters } from "./Filters";
+import { useDetectGPU } from "@react-three/drei";
 
 type TProps = Partial<RouterProps>;
 export const Router: FC<TProps> = () => {
+  const { isMobile, tier } = useDetectGPU();
   const isPointer = useNoPointer();
   return (
     <_Router>
       <Filters />
       <Background />
       <Source />
-      {/* {isPointer && <Cursor />} */}
+      {isPointer && !isMobile && tier > 1 && <Cursor />}
     </_Router>
   );
 };
- 
