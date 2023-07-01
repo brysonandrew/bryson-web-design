3ff6607613f733e8134542dd3603720bb0a250fb
Andrew <andrewbryson12@gmail.com>
2023-07-01 23:40:45 +1200

    Added back build step

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 import windiCss from "vite-plugin-windicss";
 
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   plugins: [
     windiCss(),
     react({
