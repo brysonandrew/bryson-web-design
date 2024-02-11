2bfec89d0391b476732913480b390e50732b12cd
Andrew <andrewbryson12@gmail.com>
2024-02-11 17:25:21 +1300

    A few lib changes

--- config/uno/shortcuts/base.ts
+++ config/uno/shortcuts/base.ts
@@ -1,6 +1,8 @@
 import { StaticShortcutMap } from 'unocss';
 
 export const BASE: StaticShortcutMap = {
+  'w-core':
+    'px-4 w-full sm:w-sm md:w-md lg:w-lg xl:w-xl xxl:w-xxl',
   'gap-box': 'gap-6',
   'gap-box-1_5': 'gap-8',
   'gap-box-2': 'gap-12',
--- lib/uno/shortcuts/layout.ts
+++ lib/uno/shortcuts/layout.ts
@@ -1,6 +1,4 @@
 export const LAYOUT = {
-  'w-core':
-    'px-4 w-full sm:w-sm md:w-md lg:w-lg xl:w-xl xxl:w-xxl',
   fill: 'absolute inset-0',
   'fill-1': 'absolute -inset-1',
   'fill-2': 'absolute -inset-2',
