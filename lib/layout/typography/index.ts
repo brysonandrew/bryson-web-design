2acab2f6448622130913ada5f104e603823faed8
Andrew <andrewbryson12@gmail.com>
2024-03-12 23:27:48 +1300

    update

--- lib/layout/typography/index.ts
+++ lib/layout/typography/index.ts
@@ -1,9 +1,9 @@
 
 export * from './Letter';
 export * from './Sub';
-export * from './title/Content';
-export * from './title';
 export * from './animation/ShiftUp';
 export * from './animation/Stagger';
 export * from './animation/Word';
 export * from './animation';
+export * from './title/Content';
+export * from './title';
--- lib/uno/theme/index.ts
+++ lib/uno/theme/index.ts
@@ -54,3 +54,4 @@ export const resolveTheme = <T extends Theme>(
 
 
 
+
