9b9b72ebaf23564a9ee6080c57d4d98b050b58a5
Andrew <andrewbryson12@gmail.com>
2024-06-29 23:24:41 +1200

    settings buttons

--- lib/interactive/button/icon/Sun.tsx
+++ lib/interactive/button/icon/Sun.tsx
@@ -11,8 +11,8 @@ export const Sun: FC<TProps> = ({
 }) => (
   <motion.svg
     className={clsx(classValue)}
-    width='29'
-    height='29'
+    width='26'
+    height='26'
     viewBox='0 0 50 50'
     xmlns='http://www.w3.org/2000/svg'
     fill='currentColor'
