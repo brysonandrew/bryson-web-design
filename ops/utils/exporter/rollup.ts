323e9aa2c7450ab228f5382f99b434f127816537
Andrew <andrewbryson12@gmail.com>
2024-11-06 00:41:27 +0100

    removed unused packages

--- ops/utils/exporter/rollup.ts
+++ ops/utils/exporter/rollup.ts
@@ -1,15 +1,27 @@
-import type { RollupOptions } from "rollup";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
-import dts from "rollup-plugin-dts";
-import ts from "@rollup/plugin-typescript";
+import type { RollupOptions } from 'rollup';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import ts from '@rollup/plugin-typescript';
 
-import type { TInput } from "../../config/types/z-exporter";
+import type { TInput } from '../../config/types/z-exporter';
 
-export const rollupConfig = ({ input, output }: TInput): RollupOptions => ({
+export const rollupConfig = ({
   input,
-  plugins: [nodeResolve(), commonjs(), ts({ compilerOptions: { module: "none" } }), dts(), json()],
-  external: ["react", "react-dom", "next", "styled-components"],
+  output,
+}: TInput): RollupOptions => ({
+  input,
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    ts({ compilerOptions: { module: 'none' } }),
+    json(),
+  ],
+  external: [
+    'react',
+    'react-dom',
+    'next',
+    'styled-components',
+  ],
   output,
 });
