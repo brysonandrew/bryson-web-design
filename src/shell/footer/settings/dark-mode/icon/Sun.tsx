83d8daa7f10bc80219260cd0f6b6e74bea863b71
brysonandrew <andrewbryson12@gmail.com>
2025-12-01 08:15:38 +0100

    bigger sun

--- src/shell/footer/settings/dark-mode/icon/Sun.tsx
+++ src/shell/footer/settings/dark-mode/icon/Sun.tsx
@@ -12,8 +12,8 @@ export const Sun: FC<TProps> = ({
 }) => (
   <motion.svg
     className={cx(classValue)}
-    width="14"
-    height="14"
+    width="16"
+    height="16"
     viewBox="0 0 50 50"
     xmlns="http://www.w3.org/2000/svg"
     fill="currentColor"
