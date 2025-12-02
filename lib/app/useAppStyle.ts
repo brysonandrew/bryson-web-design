e6b906c9e71571fe59b876a62d66473a4d3d6862
brysonandrew <andrewbryson12@gmail.com>
2025-12-02 19:26:13 +0100

    tried and failed

--- lib/app/useAppStyle.ts
+++ lib/app/useAppStyle.ts
@@ -3,27 +3,32 @@ import { DEFAULT_STYLE } from './config/constants/style';
 import { mergeDeepObjects } from '@brysonandrew/utils-object';
 import { TAnyRecord } from '@brysonandrew/config-types';
 import { TDefaultStyle } from '@brysonandrew/app/config/types';
+import { useDarkMode } from '@brysonandrew/dark-mode';
+// import { CUSTOM_STYLE } from '@app/style';
+
+// type Mutable<T> = {
+//   -readonly [P in keyof T]: T[P];
+// };
 
 type TConfig<S extends TAnyRecord> = { style: S };
 export const useAppStyle = <S extends TAnyRecord>({
   style,
 }: TConfig<S>) => {
   type TStyle = S & TDefaultStyle;
-  // const { isDarkMode } = useDarkMode();
+  const { isDarkMode } = useDarkMode();
 
-  const appStyle = useMemo(() => {
-    // style.COLOR.accent = isDarkMode
-    //   ? style.COLOR.accent
-    //   : '#0F766E';
+  const appStyle = useMemo<TStyle>(() => {
     const result = mergeDeepObjects<TStyle>(
       DEFAULT_STYLE as TStyle,
       style,
     );
 
     return result;
-  }, [
-    // isDarkMode
-  ]);
+  }, []);
+
+  // (appStyle as Mutable<TStyle>).COLOR.accent = isDarkMode
+  //   ? CUSTOM_STYLE.COLOR.accent
+  //   : '#4A63D9';
 
   return appStyle;
 };
